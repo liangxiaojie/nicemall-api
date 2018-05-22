852180a8516437fe9d9814b9cb31b088d0188933
liangxiaojie <15201515601@163.com>
2018-05-22 13:56:12 +0800

    wx login

--- app/controller/auth.js
+++ app/controller/auth.js
@@ -1,5 +1,26 @@
 'use strict';
 
+exports.wxLogin = async ctx => {
+  const body = ctx.request.body;
+  const appId = 'wx_gz_config.appid';
+  const successUrl = body.successUrl;
+  const redirectUrl = '/index/wx/wxInfo.html?successUrl=' + successUrl;
+
+  // snsapi_userinfo （弹出授权页面，可通过openid拿到昵称、性别、所在地。并且，即使在未关注的情况下，只要用户授权，也能获取其信息）
+  const url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + appId
+  + '&redirect_uri='.urlencode(redirectUrl)
+  + '&response_type=code'
+  + '&scope=snsapi_userinfo'
+  + '&state=xx'
+  + '#wechat_redirect';
+
+  ctx.body = {
+    code: '0',
+    message: 'success',
+    data: { url },
+  };
+};
+
 exports.login = async ctx => {
   const createRule = {
     name: {
--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
+  app.post('/api/auth/wxLogin', controller.auth.wxLogin);
   app.post('/api/auth/login', controller.auth.login);
   app.post('/api/auth/register', controller.auth.register);
   app.resources('users', '/api/users', controller.user);
