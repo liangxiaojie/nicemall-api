ecc143cd80d1cc81400e15f19e660bb5655ad826
liangxiaojie <15201515601@163.com>
2018-06-05 14:20:54 +0800

    wxToken

--- app.config.js
+++ app.config.js
@@ -2,7 +2,10 @@
 const Domain = process.env.Domain || 'localhost';
 const API_URL = process.env.API_URL || 'http://localhost:7001';
 
+const wxToken = 'asdhfsjsjkdfjkjjjkj';
+
 module.exports = {
   Domain,
   API_URL,
+  wxToken,
 };
--- app/controller/wx.js
+++ app/controller/wx.js
@@ -2,6 +2,7 @@
 
 const Controller = require('egg').Controller;
 const crypto = require('crypto');
+const { wxToken } = require('../../app.config');
 
 class wxController extends Controller {
   async index() {
@@ -27,15 +28,13 @@ class wxController extends Controller {
 
     // 1.获取微信服务器Get请求的参数 signature、timestamp、nonce、echostr
     const { signature, timestamp, nonce, echostr } = query;
-    const token = 'asdhfsjsjkdfjkjjjkj';
 
-    // 2.将token、timestamp、nonce三个参数进行字典序排序
-    const array = [ token, timestamp, nonce ].sort();
+    // 2.将token、timestamp、nonce三个参数进行字典序排序并拼接成一个字符串
+    const str = [ wxToken, timestamp, nonce ].sort().join('');
 
-    // 3.将三个参数字符串拼接成一个字符串进行sha1加密
-    const tempStr = array.join('');
+    // 3.进行sha1加密
     const hashCode = crypto.createHash('sha1'); // 创建加密类型
-    const resultCode = hashCode.update(tempStr, 'utf8').digest('hex'); // 对传入的字符串进行加密
+    const resultCode = hashCode.update(str, 'utf8').digest('hex'); // 对传入的字符串进行加密
 
     // 4.开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
     if (resultCode === signature) {
