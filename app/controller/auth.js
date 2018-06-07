1b0ddbfcbe25faaf01bb88f007c42dd79168be1a
liangxiaojie <15201515601@163.com>
2018-06-07 21:57:58 +0800

    auth

--- app/controller/auth.js
+++ app/controller/auth.js
@@ -1,103 +1,113 @@
 'use strict';
 
-exports.wxLogin = async ctx => {
-  const body = ctx.request.body;
-  const appId = 'wx_gz_config.appid';
-  const successUrl = body.successUrl;
-  const redirectUrl = '/index/wx/wxInfo.html?successUrl=' + successUrl;
+const Controller = require('egg').Controller;
+const { wxConfig } = require('../../app.config');
 
-  // snsapi_userinfo （弹出授权页面，可通过openid拿到昵称、性别、所在地。并且，即使在未关注的情况下，只要用户授权，也能获取其信息）
-  const url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + appId
-  + '&redirect_uri=' + encodeURIComponent(redirectUrl)
-  + '&response_type=code'
-  + '&scope=snsapi_userinfo'
-  + '&state=xx'
-  + '#wechat_redirect';
+class authController extends Controller {
+  async wxLogin() {
+    const { ctx } = this;
+    const { successUrl } = ctx.request.body;
 
-  ctx.body = {
-    code: '0',
-    message: 'success',
-    data: { url },
-  };
-};
+    // snsapi_userinfo （弹出授权页面，可通过openid拿到昵称、性别、所在地。并且，即使在未关注的情况下，只要用户授权，也能获取其信息）
+    const url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + wxConfig.appid +
+      '&redirect_uri=' + encodeURIComponent(successUrl) +
+      '&response_type=code' +
+      '&scope=snsapi_userinfo' +
+      '&state=xx' +
+      '#wechat_redirect';
 
-exports.login = async ctx => {
-  const createRule = {
-    username: {
-      type: 'string',
-    },
-    password: {
-      type: 'string',
-    },
-  };
-  // 校验参数
-  ctx.validate(createRule);
-
-  const { username, password } = ctx.request.body;
-
-  const user = await ctx.model.User.findOne({
-    username,
-    password,
-  });
-
-  if (!user) {
     ctx.body = {
-      success: false,
-      message: '用户名或密码错误',
+      success: true,
+      message: 'success',
+      data: { url },
     };
-    return;
   }
 
-  const data = {
-    username: user.username,
-    roles: user.roles,
-  };
-
-  ctx.session.user = data;
-  ctx.body = {
-    success: true,
-    message: '登录成功',
-    data,
-  };
-};
-
-exports.userinfo = async ctx => {
-  ctx.body = {
-    success: true,
-    message: '',
-    data: ctx.session.user,
-  };
-};
-
-exports.logout = async ctx => {
-  ctx.session.user = null;
-  ctx.body = {
-    success: true,
-    message: '',
-    data: null,
-  };
-};
-
-exports.register = async ctx => {
-  const createRule = {
-    name: {
-      type: 'string',
-    },
-    password: {
-      type: 'string',
-    },
-  };
-  // 校验参数
-  ctx.validate(createRule);
-
-  const user = await ctx.model.User.create(ctx.request.body);
-  ctx.body = {
-    code: 0,
-    message: 'success',
-    data: {
-      user: {
-        name: user.name,
+  async login() {
+    const { ctx } = this;
+    const createRule = {
+      username: {
+        type: 'string',
       },
-    },
-  };
-};
+      password: {
+        type: 'string',
+      },
+    };
+    // 校验参数
+    ctx.validate(createRule);
+
+    const { username, password } = ctx.request.body;
+
+    const user = await ctx.model.User.findOne({
+      username,
+      password,
+    });
+
+    if (!user) {
+      ctx.body = {
+        success: false,
+        message: '用户名或密码错误',
+      };
+      return;
+    }
+
+    const data = {
+      username: user.username,
+      roles: user.roles,
+    };
+
+    ctx.session.user = data;
+    ctx.body = {
+      success: true,
+      message: '登录成功',
+      data,
+    };
+  }
+
+  async userinfo() {
+    const { ctx } = this;
+    ctx.body = {
+      success: true,
+      message: '',
+      data: ctx.session.user,
+    };
+  }
+
+  async logout() {
+    const { ctx } = this;
+    ctx.session.user = null;
+    ctx.body = {
+      success: true,
+      message: '',
+      data: null,
+    };
+  }
+
+  async register() {
+    const { ctx } = this;
+    const createRule = {
+      name: {
+        type: 'string',
+      },
+      password: {
+        type: 'string',
+      },
+    };
+    // 校验参数
+    ctx.validate(createRule);
+
+    const user = await ctx.model.User.create(ctx.request.body);
+    ctx.body = {
+      code: 0,
+      message: 'success',
+      data: {
+        user: {
+          name: user.name,
+        },
+      },
+    };
+  }
+
+}
+
+module.exports = authController;
--- app/router.js
+++ app/router.js
@@ -5,15 +5,21 @@
  */
 module.exports = app => {
   const { router, controller } = app;
+
   router.get('/', controller.home.index);
+
+  // router.post('/api/auth/register', controller.auth.register);
   router.post('/api/auth/wxLogin', controller.auth.wxLogin);
   router.post('/api/auth/login', controller.auth.login);
-  // router.post('/api/auth/register', controller.auth.register);
+  router.post('/api/auth/logout', controller.auth.logout);
   router.get('/api/auth/userinfo', controller.auth.userinfo);
+
   router.resources('users', '/api/users', controller.user);
+
   router.post('/api/uploads', controller.upload.upload);
 
   router.get('/api/wx', controller.wx.index);
   router.post('/api/wx', controller.wx.message);
   router.get('/api/wx/createMenu', controller.wx.createMenu);
+
 };
