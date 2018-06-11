e3703dbd3b48448ed1b370b6ede460402cc546a2
liangxiaojie <15201515601@163.com>
2018-06-11 16:41:12 +0800

    await

--- app/controller/auth.js
+++ app/controller/auth.js
@@ -47,19 +47,19 @@ class authController extends Controller {
     let user;
 
     if (query.openid) {
-      user = ctx.model.User.findOne({ openid: query.openid });
+      user = await ctx.model.User.findOne({ openid: query.openid });
     } else {
       let res = await request(`https://api.weixin.qq.com/sns/oauth2/access_token?appid=${wxConfig.appid}&secret=${wxConfig.secret}&code=${code}&grant_type=authorization_code`);
       res = JSON.parse(res);
       // const { access_token, expires_in, refresh_token, openid, scope } = res;
       const { access_token, openid } = res;
-      user = ctx.model.User.findOne({ openid });
+      user = await ctx.model.User.findOne({ openid });
       if (!user) {
         let userinfo = await request(`https://api.weixin.qq.com/sns/userinfo?access_token=${access_token}&openid=${openid}&lang=zh_CN`);
         userinfo = JSON.parse(userinfo);
         if (userinfo.openid) {
           const { nickname, sex, province, city, country, headimgurl, privilege, unionid } = userinfo;
-          ctx.model.User.create({
+          await ctx.model.User.create({
             openid, nickname, sex, province, city, country, headimgurl, privilege, unionid,
           });
         }
