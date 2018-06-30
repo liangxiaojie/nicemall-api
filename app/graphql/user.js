c6f2eea96bd6500a7229df356bb49bcc44c71f3c
liangxiaojie <15201515601@163.com>
2018-06-30 19:23:40 +0800

    cartGoods

--- app/graphql/user.js
+++ app/graphql/user.js
@@ -2,18 +2,16 @@
 
 exports.resolver = {
   Query: {
-    wxUserinfo(obj, { code }, ctx) {
+    async wxUserinfo(obj, { code }, ctx) {
       let user;
 
-      if (ctx.isAuthenticated() && ctx.user) {
+      if (ctx.user) {
         user = ctx.user;
       } else if (code) {
-        user = ctx.service.wxUser.getWxUserByCode(code);
+        user = await ctx.service.wxUser.getWxUserByCode(code);
         ctx.login(user);
-      }
-
-      if (process.env.NODE_ENV === 'development') {
-        user = ctx.service.wxUser.getWxUserById('5b2e0dc92d2b644a6c18ed7e');
+      } else if (process.env.NODE_ENV === 'development') {
+        user = await ctx.service.wxUser.getWxUserById('5b2e0dc92d2b644a6c18ed7e');
         ctx.login(user);
       }
 
